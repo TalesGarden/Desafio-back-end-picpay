6c8dff19dd559a41ca45398f00d4ead9de8e3970
tales rodrigues jardim <talesjardim@gmail.com>
2024-06-23 00:21:38 -0300

    cria entidades principais projeto

--- src/main/java/projeto/pic/com/picpay/PicpayApplication.java
+++ src/main/java/projeto/pic/com/picpay/PicpayApplication.java
@@ -1,9 +1,11 @@
 package projeto.pic.com.picpay;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PicpayApplication {
 
 	public static void main(String[] args) {
